d99f9577ce80b9fac4b057a995d7a9627ebee8e4
David <Davida.fagan@gmail.com>
2023-03-23 14:20:03 -0700

    Video Background Added
    Video Background is added and functions.  Need to update to ensure background flexs with video in future commit.

--- ComposureWatch/src/Components/Body.jsx
+++ ComposureWatch/src/Components/Body.jsx
@@ -1,7 +1,5 @@
 const Body = (props) => {
-  const classes =
-    "w-screen h-screen text-[#1d253a] bg-gradient-to-r from-[#66c4ff]  via-[#33b1ff]to-[#009dff]" +
-    props.className;
+  const classes = "w-screen h-screen text-[#1d253a]" + props.className;
 
   return <div className={classes}>{props.children}</div>;
 };
--- ComposureWatch/src/Components/TitleSection.jsx
+++ ComposureWatch/src/Components/TitleSection.jsx
@@ -1,12 +1,27 @@
 const TitleSection = () => {
   return (
-    <div className="bg-purple-800 flex flex-col justify-center items-center gap-y-4 uppercase text-white py-80 px-40">
-      <h2 className="text-3xl font-bold">ComposureWatch</h2>
-      <h3 className="text-3xl italic">A team composition worth fighting for</h3>
-      <p>Team-Based planning &#x2022; free to use</p>
-      <button className="bg-orange-600 w-30 h-15 px-6 py-3 uppercase font-semibold rounded-sm opacity-80 hover:opacity-100">
-        Try now
-      </button>
+    <div>
+      <div className="relative">
+        <video
+          autoPlay
+          loop
+          muted
+          className="absolute z-[-10] w-auto 
+              min-w-full min-h-full max-w-none"
+        >
+          <source src="src/video/zenHighlight.mp4" type="video/mp4" />
+        </video>
+      </div>
+      <div className="bg-[#66c4ff] bg-opacity-25 text-white p-80 gap-y-8 uppercaseflex flex-col justify-center items-center">
+        <h2 className="text-3xl font-bold">ComposureWatch</h2>
+        <h3 className="text-3xl italic pb-2">
+          A team composition worth fighting for
+        </h3>
+        <p>Team-Based planning &#x2022; free to use</p>
+        <button className="bg-orange-600 w-30 h-15 px-6 py-3 uppercase font-semibold rounded-sm opacity-80 hover:opacity-100">
+          Try now
+        </button>
+      </div>
     </div>
   );
 };
