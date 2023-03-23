8a72bfb9d093ee94ecb10aa12b03ed803edc3160
David <Davida.fagan@gmail.com>
2023-03-23 12:29:45 -0700

    Added Card Styling
    Added tailwind stylization to card.  Added two layers of portrait shadow as well as padding and font color alterations.

--- ComposureWatch/src/Components/PlayerCards.jsx
+++ ComposureWatch/src/Components/PlayerCards.jsx
@@ -6,12 +6,19 @@ const PlayerCards = (props) => {
   return (
     <div className="bg-black p-8">
       <div className="border-4 border-[#2EF3FF]  rounded-xl shadow-2xl shadow-[#13336C] bg-[#0A0D30]">
-        <div className="border-2 border-[#2EF3FF] bg-[#0A0D30] h-96 w-72 rounded-lg  m-0.5 flex flex-col justify-start items-center">
-          <div className="flex flex-col justify-center items-center border-2 bg-black border-[#2EF3FF] border-opacity-75 rounded-lg px-24 py-32 m-0.5 text-[#2EF3FF] shadow-inner shadow-[#13336C]">
-            <p>{props.character}</p>
-            <p className="italic">{props.name}</p>
+        <div className="border-2 border-[#2EF3FF] bg-[#0A0D30] rounded-lg  m-0.5 flex flex-col justify-start items-center">
+          <div className="flex flex-col justify-start items-center border-2 bg-black border-[#2EF3FF] border-opacity-75 rounded-lg px-20 py-6  m-0.5 mb-12 text-[#2EF3FF] shadow-inner shadow-[#13336C]">
+            <div className="rounded-xl shadow-xl shadow-[#13336C] m-0.5 mb-10">
+              <img
+                src="src/IMG/portrait-moira.png"
+                alt="charcter portrait"
+                className="border-2 border-black rounded-xl shadow-2xl shadow-[#13336C]"
+              />
+            </div>
+            {/* <p>{props.character}</p> */}
+            <h2 className="italic text-3xl leading-10">{props.name}</h2>
             {/* <div>{cardImg}</div> */}
-            <p>{props.rank}</p>
+            <h3 className="text-white">{props.rank}</h3>
           </div>
         </div>
       </div>
