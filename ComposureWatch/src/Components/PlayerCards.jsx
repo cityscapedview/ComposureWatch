f969d176fdc23c12027ec9e14ecbf79f712f2fa9
David <Davida.fagan@gmail.com>
2023-03-23 11:14:29 -0700

    Updated Styling for PlayerCards
    Added tailwind styling to PlayerCards.

--- ComposureWatch/src/Components/PlayerCards.jsx
+++ ComposureWatch/src/Components/PlayerCards.jsx
@@ -4,12 +4,16 @@ const PlayerCards = (props) => {
   }
 
   return (
-    <div className="border-4 border-[#2EF3FF] bg-[#010206] rounded-lg ">
-      <div className="border-2 border-[#2EF3FF] h-96 w-72 rounded-lg flex flex-col justify-center items-center m-1">
-        <p>{props.name}</p>
-        {/* <div>{cardImg}</div> */}
-        <p>{props.character}</p>
-        <p>{props.rank}</p>
+    <div className="bg-black p-8">
+      <div className="border-4 border-[#2EF3FF]  rounded-xl shadow-2xl shadow-[#13336C] bg-[#0A0D30]">
+        <div className="border-2 border-[#2EF3FF] bg-[#0A0D30] h-96 w-72 rounded-lg  m-0.5 flex flex-col justify-start items-center">
+          <div className="flex flex-col justify-center items-center border-2 bg-black border-[#2EF3FF] border-opacity-75 rounded-lg px-24 py-32 m-0.5 text-[#2EF3FF] shadow-inner shadow-[#13336C]">
+            <p>{props.character}</p>
+            <p className="italic">{props.name}</p>
+            {/* <div>{cardImg}</div> */}
+            <p>{props.rank}</p>
+          </div>
+        </div>
       </div>
     </div>
   );
