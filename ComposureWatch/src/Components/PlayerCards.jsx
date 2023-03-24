9a85df94efc40acd96c7ad7c8cb6423ce4ead62c
David <Davida.fagan@gmail.com>
2023-03-24 09:46:20 -0700

    Added basic conditional rendering
    Added most basic form of conditional rendering.  This will provide the start of obtaining character profile images from a new object based on selected character.

--- ComposureWatch/src/Components/PlayerCards.jsx
+++ ComposureWatch/src/Components/PlayerCards.jsx
@@ -1,6 +1,17 @@
 const PlayerCards = (props) => {
-  if (props.name == "moira") {
-    return (cardImg = <img src="src/IMG/portrait-moira.png" />);
+  let charImg;
+  if (props.character == "Moira") {
+    charImg = (
+      <div className="rounded-xl shadow-xl shadow-[#13336C] m-0.5 mb-10">
+        <img
+          src="src/IMG/portrait-moira.png"
+          alt="charcter portrait"
+          className="border-2 border-black rounded-xl shadow-2xl shadow-[#13336C]"
+        />
+      </div>
+    );
+  } else {
+    charImg = <h1>No Character Image Available.</h1>;
   }
 
   return (
@@ -8,13 +19,7 @@ const PlayerCards = (props) => {
       <div className="border-4 border-[#2EF3FF]  rounded-xl shadow-2xl shadow-[#13336C] bg-[#0A0D30]">
         <div className="border-2 border-[#2EF3FF] bg-[#0A0D30] rounded-lg  m-0.5 flex flex-col justify-start items-center">
           <div className="flex flex-col justify-start items-center border-2 bg-black border-[#2EF3FF] border-opacity-75 rounded-lg px-20 py-6  m-0.5 mb-12 text-[#2EF3FF] shadow-inner shadow-[#13336C]">
-            <div className="rounded-xl shadow-xl shadow-[#13336C] m-0.5 mb-10">
-              <img
-                src="src/IMG/portrait-moira.png"
-                alt="charcter portrait"
-                className="border-2 border-black rounded-xl shadow-2xl shadow-[#13336C]"
-              />
-            </div>
+            {charImg}
             {/* <p>{props.character}</p> */}
             <h2 className="italic text-3xl leading-10">{props.name}</h2>
             {/* <div>{cardImg}</div> */}
