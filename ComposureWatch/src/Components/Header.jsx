1b48dd3af4a52338b630b52a50b3263b81faddb2
David <Davida.fagan@gmail.com>
2023-03-28 12:00:31 -0700

    Added flex to navbar

--- ComposureWatch/src/Components/Header.jsx
+++ ComposureWatch/src/Components/Header.jsx
@@ -2,10 +2,35 @@ const Header = () => {
   return (
     <div className="sticky top-2 mx-auto">
       <header className="mx-4 px-12 py-4 flex justify-start items-end space-x-6 border rounded-lg text-black bg-[rgba(229,235,244,0.8)]">
-        <h1 className="font-semibold text-4xl">ComposureWatch</h1>
-        <h2 className="font-semibold text-2xl">
-          A team balance worth fighting for
-        </h2>
+        <div>
+          <ul className="flex flex-row justify-start">
+            <li>
+              <a className="block py-2 pl-3 pr-4" href="#">
+                Game Info
+              </a>
+            </li>
+            <li>
+              <a className="block py-2 pl-3 pr-4" href="#">
+                Heros
+              </a>
+            </li>
+            <li>
+              <a className="block py-2 pl-3 pr-4" href="#">
+                Season 3
+              </a>
+            </li>
+            <li>
+              <a className="block py-2 pl-3 pr-4" href="#">
+                News
+              </a>
+            </li>
+            <li>
+              <a className="block py-2 pl-3 pr-4" href="#">
+                Community
+              </a>
+            </li>
+          </ul>
+        </div>
       </header>
     </div>
   );
