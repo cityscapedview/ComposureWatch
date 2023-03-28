1ec2b252acaa5ea199ffa842ea144798eb3a5381
David <Davida.fagan@gmail.com>
2023-03-28 12:40:08 -0700

    Modified styling

--- ComposureWatch/src/Components/TitleSection.jsx
+++ ComposureWatch/src/Components/TitleSection.jsx
@@ -1,18 +1,18 @@
 const TitleSection = () => {
   return (
     <div>
-      <div className="relative">
+      <div className="z-[-10] absolute top-0 left-0">
         <video
           autoPlay
           loop
           muted
-          className="absolute z-[-10] w-auto 
+          className="w-auto 
               min-w-full min-h-full max-w-none"
         >
           <source src="src/video/zenHighlight.mp4" type="video/mp4" />
         </video>
       </div>
-      <div className="bg-[#66c4ff] bg-opacity-25 text-white p-80 gap-y-8 uppercaseflex flex-col justify-center items-center">
+      <div className="bg-[#66c4ff] bg-opacity-25 text-white gap-y-8 uppercase flex flex-col justify-center items-center">
         <h2 className="text-3xl font-bold">ComposureWatch</h2>
         <h3 className="text-3xl italic pb-2">
           A team composition worth fighting for
