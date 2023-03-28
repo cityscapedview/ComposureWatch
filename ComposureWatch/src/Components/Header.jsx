6e4f460043e5046e55601f50659fdcbb4b157144
David <Davida.fagan@gmail.com>
2023-03-28 11:21:37 -0700

    Added sticky position
    Added sticky position to nav bar for floating functionality.

--- ComposureWatch/src/Components/Header.jsx
+++ ComposureWatch/src/Components/Header.jsx
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <div>
+    <div className="sticky top-2 mx-auto">
       <header className="mx-4 px-12 py-4 flex justify-start items-end space-x-6 border rounded-lg text-black bg-[rgba(229,235,244,0.8)]">
         <h1 className="font-semibold text-4xl">ComposureWatch</h1>
         <h2 className="font-semibold text-2xl">
