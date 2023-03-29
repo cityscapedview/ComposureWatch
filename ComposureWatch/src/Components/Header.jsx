e92e89ea4a29231da5ef0182085aeb5eabcfaa69
David <Davida.fagan@gmail.com>
2023-03-28 17:21:20 -0700

    Updated NavBar Styling
    Added button to navbar.  Logo, relative positioning, and  flex need to be adjusted in further commits on this branch.

--- ComposureWatch/src/Components/Header.jsx
+++ ComposureWatch/src/Components/Header.jsx
@@ -3,16 +3,27 @@ import HeaderListItem from "./HeaderListItem";
 const Header = () => {
   return (
     <div className="sticky top-4 mx-auto">
-      <header className="mx-4 px-12 py-3 flex justify-start items-end space-x-6 border rounded-lg text-black bg-[rgba(229,235,244,0.8)]">
+      <header className="mx-4 px-12 py-3 flex justify-start items-end space-x-6 border rounded-lg text-black bg-[rgba(229,235,244,0.95)] relative">
+        <div className="bg-white text-blue-400 px-5 pt-4 h-full absolute left-0 top-0 font-bold text-xl uppercase tracking-tighter rounded-l-lg">
+          Blissard
+        </div>
+        <img
+          src="src/images/yinyang.jpg"
+          alt="yin-yang logo"
+          className="pl-20 h-10"
+        />
         <div>
           <ul className="flex flex-row justify-start">
             <HeaderListItem info="Game Info" />
-            <HeaderListItem info="Heros" />
+            <HeaderListItem info="Heroes" />
             <HeaderListItem info="Season 3" />
             <HeaderListItem info="News" />
             <HeaderListItem info="Community" />
           </ul>
         </div>
+        <button className="bg-orange-600  px-3 py-2 ml-6 uppercase font-semibold rounded-md opacity-80 text-white hover:opacity-100">
+          Try now
+        </button>
       </header>
     </div>
   );
--- ComposureWatch/src/Components/HeaderListItem.jsx
+++ ComposureWatch/src/Components/HeaderListItem.jsx
@@ -1,7 +1,10 @@
 const HeaderListItem = (props) => {
   return (
     <li>
-      <a className="block py-2 pl-3 pr-4" href="#">
+      <a
+        className="block py-2 pl-3 pr-4 text-lg leading-5 font-medium text-left"
+        href="#"
+      >
         {props.info}
       </a>
     </li>
