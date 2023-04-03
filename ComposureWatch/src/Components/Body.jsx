1584e916a13985c4609f9c519f78bd39072b2fa4
David <Davida.fagan@gmail.com>
2023-04-03 12:25:19 -0700

    Video-background-styling
    Updated video Styling background.

--- ComposureWatch/src/Components/Body.jsx
+++ ComposureWatch/src/Components/Body.jsx
@@ -1,5 +1,7 @@
 const Body = (props) => {
-  const classes = "w-screen h-screen text-[#1d253a]" + props.className;
+  const classes =
+    "w-screen h-screen text-[#1d253a]p-0 m-0 box-border block" +
+    props.className;
 
   return <div className={classes}>{props.children}</div>;
 };
--- ComposureWatch/src/Components/TitleSection.jsx
+++ ComposureWatch/src/Components/TitleSection.jsx
@@ -1,18 +1,12 @@
 const TitleSection = () => {
   return (
     <div>
-      <div className="z-[-10] absolute top-0 left-0">
-        <video
-          autoPlay
-          loop
-          muted
-          className="w-auto 
-              min-w-full min-h-full max-w-none"
-        >
+      <div className="z-[-1] absolute top-0 left-0 right-0 bottom-0 overflow-hidden">
+        <video autoPlay loop muted className="object-fill w-full h-full">
           <source src="src/video/zenHighlight.mp4" type="video/mp4" />
         </video>
       </div>
-      <div className="bg-[#66c4ff] bg-opacity-25 text-white gap-y-8 uppercase flex flex-col justify-center items-center">
+      <div className="bg-opacity-25 text-white gap-y-8 uppercase flex flex-col justify-center items-center h-[91vh]">
         <h2 className="text-3xl font-bold">ComposureWatch</h2>
         <h3 className="text-3xl italic pb-2">
           A team composition worth fighting for
