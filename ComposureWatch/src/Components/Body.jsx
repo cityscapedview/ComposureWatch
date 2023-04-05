b93774211e70c3861008140587136b8061f29e21
David <Davida.fagan@gmail.com>
2023-04-05 14:17:33 -0700

    Deconstruction Body Component
    Deconstructed props going into body component for clarity.

--- ComposureWatch/src/Components/Body.jsx
+++ ComposureWatch/src/Components/Body.jsx
@@ -1,7 +1,8 @@
-const Body = (props) => {
-  const classes = "text-[#1d253a] p-0 m-0 box-border block " + props.className;
+const Body = ({ children }) => {
+  const classes =
+    "text-[#1d253a] p-0 m-0 box-border block " + children.className;
 
-  return <div className={classes}>{props.children}</div>;
+  return <div className={classes}>{children}</div>;
 };
 
 export default Body;
