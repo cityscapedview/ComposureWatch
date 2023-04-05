fab343065d708d6b315b5b671b72a389d7fc914c
David <Davida.fagan@gmail.com>
2023-04-05 16:50:58 -0700

    Created SVG folder
    +Created SVG folder for good housekeeping and organization.
    +renamed chevron, user, and search SVGs to include svg in title.
    +Deconstructed props in header list items to accept Info.

--- ComposureWatch/src/Components/Header/HeaderListItemChevron.jsx
+++ ComposureWatch/src/Components/Header/HeaderListItemChevron.jsx
@@ -1,15 +1,15 @@
-import Chevron from "../Chevron";
+import ChevronSvg from "../SVG/ChevronSvg";
 
-const HeaderListItemChevron = (props) => {
+const HeaderListItemChevron = ({ info }) => {
   return (
     <li className="flex flex-row items-center">
       <a
         className="block py-2 pl-3 pr-1 text-lg leading-5 font-medium text-left"
         href="#"
       >
-        {props.info}
+        {info}
       </a>
-      <Chevron />
+      <ChevronSvg />
     </li>
   );
 };
--- ComposureWatch/src/Components/SVG/ChevronSvg.jsx
+++ ComposureWatch/src/Components/SVG/ChevronSvg.jsx
@@ -1,4 +1,4 @@
-const Chevron = () => {
+const ChevronSvg = () => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -17,4 +17,4 @@ const Chevron = () => {
   );
 };
 
-export default Chevron;
+export default ChevronSvg;
