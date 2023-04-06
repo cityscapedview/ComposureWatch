3ce48e5caf9dd346d34464368c48b8bb1c2b9551
David <Davida.fagan@gmail.com>
2023-04-06 15:13:03 -0700

    HeaderListItemChevron responsive
    Added responsive visibility to HeaderListItemChevron.

--- ComposureWatch/src/Components/Header/HeaderListItemChevron.jsx
+++ ComposureWatch/src/Components/Header/HeaderListItemChevron.jsx
@@ -2,7 +2,7 @@ import ChevronSvg from "../SVG/ChevronSvg";
 
 const HeaderListItemChevron = ({ info }) => {
   return (
-    <li className="flex flex-row items-center">
+    <li className="flex flex-row items-center invisible lg:visible">
       <a
         className="block py-2 pl-3 pr-1 text-lg leading-5 font-medium text-left"
         href="#"
