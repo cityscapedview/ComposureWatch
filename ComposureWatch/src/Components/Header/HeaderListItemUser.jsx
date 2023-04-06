f07499ea518398ae30b1ae76657067438beb695b
David <Davida.fagan@gmail.com>
2023-04-06 15:29:01 -0700

    HeaderListItem
    HeaderListItem and HeaderListItemUser changed to hidden on smaller screens and visible on larger screens.

--- ComposureWatch/src/Components/Header/HeaderListItemUser.jsx
+++ ComposureWatch/src/Components/Header/HeaderListItemUser.jsx
@@ -3,15 +3,19 @@ import UserSvg from "../SVG/UserSvg";
 
 const HeaderListItemUser = ({ info }) => {
   return (
-    <li className="flex flex-row items-center pr-2">
-      <UserSvg />
+    <li className="flex flex-row items-center justify-end lg:justify-start pr-2">
+      <div className="">
+        <UserSvg />
+      </div>
       <a
-        className="block py-2 pl-1 pr-1 text-lg leading-5 font-medium text-left"
+        className="block py-2 pl-1 pr-1 text-lg leading-5 font-medium text-left hidden lg:visible"
         href="#"
       >
         {info}
       </a>
-      <ChevronSvg />
+      <div className="hidden lg:visible">
+        <ChevronSvg />
+      </div>
     </li>
   );
 };
