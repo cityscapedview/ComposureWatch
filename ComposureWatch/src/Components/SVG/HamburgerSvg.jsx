28db61ed471ab2d9b4e6315a9b3c97dd73f72fe6
David <Davida.fagan@gmail.com>
2023-04-06 17:13:49 -0700

    HamburgerSvg Component
    +HamburgerSVG component added to nav bar at media break smaller than lg.
    +Reformatting of nav bar components for smaller screens.

---
+++ ComposureWatch/src/Components/SVG/HamburgerSvg.jsx
@@ -0,0 +1,24 @@
+const HamburgerSvg = () => {
+  return (
+    <li className="flex flex-row items-center">
+      <div className="lg:hidden">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6 stroke-[#6b7280] ml-0"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+          />
+        </svg>
+      </div>
+    </li>
+  );
+};
+
+export default HamburgerSvg;
