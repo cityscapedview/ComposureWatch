c74cc28c84279cb84dd3490f1d9e9a75c58e5d41
David <Davida.fagan@gmail.com>
2023-04-11 15:44:28 -0700

    Updated Alignment
    +Alignment adjusted to ensure text is centered and image is aligned at start.

--- ComposureWatch/src/Components/UI/PurposeSection.jsx
+++ ComposureWatch/src/Components/UI/PurposeSection.jsx
@@ -6,9 +6,9 @@ const PurposeSection = () => {
       <div
         className="grid grid-cols-1 md:grid-cols-[40fr,60fr] 
       gap-x-20 max-[1200px]:gap-x-10 gap-y-8 px-4 py-10 sm:px-6 sm:py-16 md:px-10 md:py-20
-      justify-center items-start  font-[rgb(29, 37, 58)] "
+      justify-center items-center  font-[rgb(29, 37, 58)] "
       >
-        <div className="flex flex-col items-center">
+        <div className="flex flex-col">
           <h2 className="text-6xl font-bold uppercase">A NEW ERA HAS BEGUN</h2>
           <p className="mt-6">
             ComposureWatch is a free-to-use, team-based tool made to ensure
@@ -17,7 +17,7 @@ const PurposeSection = () => {
             friends. Lorem ipsum dolor sit amet consectetur, adipisicing elit.
           </p>
         </div>
-        <div>
+        <div className="self-start">
           <img src={kiriko} alt="Kiriko Image" className="rounded" />
         </div>
       </div>
