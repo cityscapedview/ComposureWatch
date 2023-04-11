6b854d8f14d990d03771f781b6594efc0f9ca560
David <Davida.fagan@gmail.com>
2023-04-11 15:22:16 -0700

    Custom Gap Change
    +Gap change set at a custom break point with a max of 1200px.  This allows more room for content on smaller screens.

--- ComposureWatch/src/Components/UI/PurposeSection.jsx
+++ ComposureWatch/src/Components/UI/PurposeSection.jsx
@@ -5,7 +5,7 @@ const PurposeSection = () => {
     <div className="from-[#66c4ff] bg-gradient-to-tr via-[#33b1ff]to-[#009dff] flex items-center justify-center">
       <div
         className="grid grid-cols-1 md:grid-cols-[40fr,60fr] 
-      gap-x-20 gap-y-20 px-4 py-10 sm:px-6 sm:py-16 md:px-10 md:py-20
+      gap-x-20 max-[1200px]:gap-x-10 gap-y-8 px-4 py-10 sm:px-6 sm:py-16 md:px-10 md:py-20
       justify-center items-start  font-[rgb(29, 37, 58)] "
       >
         <div className="flex flex-col items-center">
