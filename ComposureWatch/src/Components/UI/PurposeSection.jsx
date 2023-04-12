d8494ddf2a5c1045f345bccc2d1b4bc6ae648ce6
David <Davida.fagan@gmail.com>
2023-04-12 14:44:13 -0700

    Max-width added to content
    +Max width of 1600 px added to content grid to ensure that content is overstretched at 4k screen size.

--- ComposureWatch/src/Components/UI/PurposeSection.jsx
+++ ComposureWatch/src/Components/UI/PurposeSection.jsx
@@ -6,10 +6,10 @@ const PurposeSection = () => {
       <div
         className="grid grid-cols-1 md:grid-cols-[40fr,60fr] 
       gap-x-20 max-[1200px]:gap-x-10 sm:gap-y-8 gap-y-6 px-4 py-10 sm:px-6 sm:py-16 md:px-10 md:py-20
-      justify-center items-center  font-[rgb(29, 37, 58)] "
+      justify-center items-center  font-[rgb(29, 37, 58)] max-w-[1600px]"
       >
         <div className="flex flex-col justify-center text-center md:text-left">
-          <h2 className="text-4xl sm:text-6xl font-bold uppercase whitespace-nowrap md:whitespace-normal">
+          <h2 className="text-4xl sm:text-6xl font-bold uppercase ">
             A NEW ERA HAS BEGUN
           </h2>
           <p className="mt-6 font-base sm:text-xl">
