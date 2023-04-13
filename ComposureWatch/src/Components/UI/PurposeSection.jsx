db73ba5dd8ffb3774b3e48ae1ddaa01c3b0ae811
David <Davida.fagan@gmail.com>
2023-04-13 15:36:03 -0700

    Zcontent component created
    Reusable formatting component for content within z styled cards.

--- ComposureWatch/src/Components/UI/PurposeSection.jsx
+++ ComposureWatch/src/Components/UI/PurposeSection.jsx
@@ -2,27 +2,23 @@ import kiriko from "../../images/UI/Kiriko.jpg";
 import Zcontentgrid from "./ZGridComponent/Zcontentgrid";
 import Zgrid from "./ZGridComponent/Zgrid";
 import Zheader from "./ZGridComponent/Zheader";
+import Zcontent from "./ZGridComponent/Zcontent";
+import Zimage from "./ZGridComponent/Zimage";
 
 const PurposeSection = () => {
   let header = "A NEW ERA HAS BEGUN";
   let content =
     "ComposureWatch is a free-to-use, team-based tool made to ensure balance amongst team creation in internal games. Nothing should get in the way of a good gaming session, especially when playing with friends. Lorem ipsum dolor sit amet consectetur, adipisicing elit.";
+  let alt = "Kiriko Image";
 
   return (
     <div className="from-[#66c4ff] bg-gradient-to-tr via-[#33b1ff]to-[#009dff] flex items-center justify-center">
       <Zgrid>
         <Zcontentgrid>
           <Zheader header={header} />
-          <p className="mt-6 font-base sm:text-xl">
-            ComposureWatch is a free-to-use, team-based tool made to ensure
-            balance amongst team creation in internal games. Nothing should get
-            in the way of a good gaming session, especially when playing with
-            friends. Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-          </p>
+          <Zcontent content={content} />
         </Zcontentgrid>
-        <div className="self-start">
-          <img src={kiriko} alt="Kiriko Image" className="rounded" />
-        </div>
+        <Zimage img={kiriko} alt={alt} />
       </Zgrid>
     </div>
   );
