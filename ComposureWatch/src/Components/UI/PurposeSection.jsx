b328208dcb2d6b0a59a5c457f7a352a8e1185595
David <Davida.fagan@gmail.com>
2023-04-13 13:36:21 -0700

    Formatting created for smaller screens
    +Added surrounding div tag to ensure that the content grid was centered properly.
    +Adjusted maximum width for content grid.

--- ComposureWatch/src/Components/UI/PurposeSection.jsx
+++ ComposureWatch/src/Components/UI/PurposeSection.jsx
@@ -6,18 +6,21 @@ const PurposeSection = () => {
       <div
         className="grid grid-cols-1 md:grid-cols-[40fr,60fr] 
       gap-x-20 max-[1200px]:gap-x-10 sm:gap-y-8 gap-y-6 px-4 py-10 sm:px-6 sm:py-16 md:px-10 md:py-20
-      justify-center items-center  font-[rgb(29, 37, 58)] max-w-[1600px]"
+      justify-items-center items-center  font-[rgb(29, 37, 58)] max-w-[1600px]"
       >
-        <div className="flex flex-col justify-center text-center md:text-left">
-          <h2 className="text-4xl sm:text-6xl font-bold uppercase ">
-            A NEW ERA HAS BEGUN
-          </h2>
-          <p className="mt-6 font-base sm:text-xl">
-            ComposureWatch is a free-to-use, team-based tool made to ensure
-            balance amongst team creation in internal games. Nothing should get
-            in the way of a good gaming session, especially when playing with
-            friends. Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-          </p>
+        <div className="flex justify-center">
+          <div className="grid grid-cols-1 justify-items-center content-center text-center max-w-[650px]  md:text-left">
+            <h2 className="text-4xl sm:text-6xl font-bold uppercase">
+              A NEW ERA HAS BEGUN
+            </h2>
+            <p className="mt-6 font-base sm:text-xl">
+              ComposureWatch is a free-to-use, team-based tool made to ensure
+              balance amongst team creation in internal games. Nothing should
+              get in the way of a good gaming session, especially when playing
+              with friends. Lorem ipsum dolor sit amet consectetur, adipisicing
+              elit.
+            </p>
+          </div>
         </div>
         <div className="self-start">
           <img src={kiriko} alt="Kiriko Image" className="rounded" />
