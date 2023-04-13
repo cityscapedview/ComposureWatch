f6482e789d405d41c97df6700d4844ec70635ee8
David <Davida.fagan@gmail.com>
2023-04-13 15:14:35 -0700

    Zheader component created
    Zheader component created as a reusable component for styling headers in the zgrid format.

--- ComposureWatch/src/Components/UI/PurposeSection.jsx
+++ ComposureWatch/src/Components/UI/PurposeSection.jsx
@@ -1,15 +1,18 @@
 import kiriko from "../../images/UI/Kiriko.jpg";
 import Zcontentgrid from "./ZGridComponent/Zcontentgrid";
 import Zgrid from "./ZGridComponent/Zgrid";
+import Zheader from "./ZGridComponent/Zheader";
 
 const PurposeSection = () => {
+  let header = "A NEW ERA HAS BEGUN";
+  let content =
+    "ComposureWatch is a free-to-use, team-based tool made to ensure balance amongst team creation in internal games. Nothing should get in the way of a good gaming session, especially when playing with friends. Lorem ipsum dolor sit amet consectetur, adipisicing elit.";
+
   return (
     <div className="from-[#66c4ff] bg-gradient-to-tr via-[#33b1ff]to-[#009dff] flex items-center justify-center">
       <Zgrid>
         <Zcontentgrid>
-          <h2 className="text-4xl sm:text-6xl font-bold uppercase">
-            A NEW ERA HAS BEGUN
-          </h2>
+          <Zheader header={header} />
           <p className="mt-6 font-base sm:text-xl">
             ComposureWatch is a free-to-use, team-based tool made to ensure
             balance amongst team creation in internal games. Nothing should get
---
+++ ComposureWatch/src/Components/UI/ZGridComponent/Zheader.jsx
@@ -0,0 +1,4 @@
+const Zheader = ({ header }) => {
+  return <h2 className="text-4xl sm:text-6xl font-bold uppercase">{header}</h2>;
+};
+export default Zheader;
