85200b54efff29527dd05fe24888c97f43e0545d
David <Davida.fagan@gmail.com>
2023-04-17 09:29:42 -0700

    Find changed to Filter
    Find filter method was replaced with filter method in Searchimg function.  The filter method was utilized as it is the recommended method in the React Documentation.

--- ComposureWatch/src/Components/Card/Icons/SearchImg.jsx
+++ ComposureWatch/src/Components/Card/Icons/SearchImg.jsx
@@ -1,6 +1,6 @@
 const SearchImg = (attribute, list) => {
-  let result = list.find((item) => item.id === attribute);
-  return result.img;
+  let result = list.filter((item) => item.id === attribute);
+  return result[0].img;
 };
 
 export default SearchImg;
