3410ba519ffac3a63c157592671506acac55e5b7
David <Davida.fagan@gmail.com>
2023-04-20 15:35:51 -0700

    Rank object created
    Created rank object to house key and values for all ranks.  It remains undetermined at this point if ranks object will remain or values will be moved to rankIcon rank object.

--- ComposureWatch/src/Components/Sorting/SortingLogic.jsx
+++ ComposureWatch/src/Components/Sorting/SortingLogic.jsx
@@ -1,139 +1,131 @@
 const SortingLogic = () => {
+  // To be determine if ranks will be part of the rank icon list or housed seperately here for values
+  let ranks = {
+    bronze5: 0.5,
+    bronze4: 0.55,
+    bronze3: 0.6,
+    bronze2: 0.65,
+    bronze1: 0.7,
+    silver5: 0.75,
+    silver4: 0.8,
+    silver3: 0.85,
+    silver2: 0.9,
+    silver1: 0.95,
+    gold5: 1,
+    gold4: 1.05,
+    gold3: 1.1,
+    gold2: 1.15,
+    gold1: 1.2,
+    platinum5: 1.25,
+    platinum4: 1.3,
+    platinum3: 1.35,
+    platinum2: 1.4,
+    platinum1: 1.45,
+    diamond5: 1.5,
+    diamond4: 1.55,
+    diamond3: 1.6,
+    diamond2: 1.65,
+    diamond1: 1.7,
+    master5: 1.75,
+    master4: 1.8,
+    master3: 1.85,
+    master2: 1.9,
+    master1: 1.95,
+    grandMaster5: 2,
+    grandMaster4: 2.05,
+    grandMaster3: 2.1,
+    grandMaster2: 2.15,
+    grandMaster1: 2.2,
+  };
 
-  let bronze5 = .5;
-  let bronze4 = .55;
-  let bronze3 = .6;
-  let bronze2 = .65;
-  let bronze1 = .70;
-  let silver5 = .75;
-  let silver4 = .8;
-  let silver3 = .85;
-  let silver2 = .9;
-  let silver1 = .95;
-  let gold5=   1;
-  let gold4=   1.05;
-  let gold3=   1.1;
-  let gold2=   1.15;
-  let gold1=   1.20;
-  let platinum5= 1.25;
-  let platinum4= 1.3;
-  let platinum3= 1.35;
-  let platinum2= 1.4;
-  let platinum1= 1.45;
-  let diamond5 = 1.5;
-  let diamond4 = 1.55;
-  let diamond3 = 1.6;
-  let diamond2 = 1.65;
-  let diamond1 = 1.7;
-  let master5 = 1.75;
-  let master4 = 1.8;
-  let master3 = 1.85;
-  let master2 = 1.9;
-  let master1 = 1.95;
-  let grandMaster5 = 2;
-  let grandMaster4 = 2.05;
-  let grandMaster3 = 2.1;
-  let grandMaster2 = 2.15;
-  let grandMaster1 = 2.2;
-  
-  
-  
+  // Below is imported card example for logic to run
   const cards = [
     {
       id: 1,
       name: "Lightster",
       character: "reaper",
       rank: "gold5",
-      rating: 1    },
+      rating: 1,
+    },
     {
       id: 2,
       name: "Mia",
       character: "Hanzo",
       rank: "gold4",
-      rating: 1.05
+      rating: 1.05,
     },
     {
       id: 3,
       name: "Ollie",
       character: "LifeWeaver",
       rank: "diamond3",
-      rating: 1.6
+      rating: 1.6,
     },
     {
       id: 4,
       name: "Zulu",
       character: "ana",
       rank: "diamond2",
-      rating: 1.65
+      rating: 1.65,
     },
     {
       id: 5,
       name: "Raihn",
       character: "reinhardt",
       rank: "silver2",
-      rating: .9
+      rating: 0.9,
     },
     {
       id: 6,
       name: "Ivysaur",
       character: "widow",
       rank: "platinum5",
-      rating: 1.25
+      rating: 1.25,
     },
     {
       id: 7,
       name: "netbattler",
       character: "ana",
       rank: "gold4",
-      rating: 1.05
+      rating: 1.05,
     },
     {
       id: 8,
       name: "Zen",
       character: "moira",
       rank: "platinum3",
-      rating: 1.35
+      rating: 1.35,
     },
     {
       id: 9,
       name: "twenty7",
       character: "pharah",
       rank: "silver1",
-      rating: .95
+      rating: 0.95,
     },
     {
       id: 10,
       name: "Gizmo",
       character: "kiriko",
       rank: "platinum1",
-      rating: 1.45
+      rating: 1.45,
     },
   ];
 
+  // Logic instructions:
 
-// Filtering for rating
+  // Filtering for rating
 
-const filtRating = cards.filter
+  const filtRating = cards.filter;
 
-let team1 = [];
-let team2 = [];
-let team1Val = 0;
-let team2Val = 0;
+  let team1 = [];
+  let team2 = [];
+  let team1Val = 0;
+  let team2Val = 0;
 
-
-  if (rating + team2Val > team1Val_ {
-    team1Val += rating;
+  if (rating + team2Val > team1Val_) {
+    return (team1Val += rating);
   }
-
-
-
-
-
-
-
-
-
-
 
   return <div>SortingLogic</div>;
 };
