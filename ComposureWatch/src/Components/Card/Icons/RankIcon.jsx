1346f1b5835bc655a5053010f88ff49487a49a01
David Fagan <Davida.fagan@gmail.com>
2023-04-25 10:42:36 -0700

    Added rating list for reference
    Rating list added for reference.  This list will be integrated into existing ranking object.

--- ComposureWatch/src/Components/Card/Icons/RankIcon.jsx
+++ ComposureWatch/src/Components/Card/Icons/RankIcon.jsx
@@ -38,6 +38,46 @@ const RankIcon = ({ rank }) => {
     },
   ];
 
+  const SortingLogic = () => {
+    let ranks = {
+      bronze5: 0.5,
+      bronze4: 0.55,
+      bronze3: 0.6,
+      bronze2: 0.65,
+      bronze1: 0.7,
+      silver5: 0.75,
+      silver4: 0.8,
+      silver3: 0.85,
+      silver2: 0.9,
+      silver1: 0.95,
+      gold5: 1,
+      gold4: 1.05,
+      gold3: 1.1,
+      gold2: 1.15,
+      gold1: 1.2,
+      platinum5: 1.25,
+      platinum4: 1.3,
+      platinum3: 1.35,
+      platinum2: 1.4,
+      platinum1: 1.45,
+      diamond5: 1.5,
+      diamond4: 1.55,
+      diamond3: 1.6,
+      diamond2: 1.65,
+      diamond1: 1.7,
+      master5: 1.75,
+      master4: 1.8,
+      master3: 1.85,
+      master2: 1.9,
+      master1: 1.95,
+      grandMaster5: 2,
+      grandMaster4: 2.05,
+      grandMaster3: 2.1,
+      grandMaster2: 2.15,
+      grandMaster1: 2.2,
+    };
+  };
+
   const SearchImg = (attribute, list) => {
     let result = list.filter((item) => item.id === attribute);
     return result[0].img;
