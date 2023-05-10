52f6539dbb50bd0f9dd8d489fd310c6ef3b78df6
David Fagan <davida.fagan@gmail.com>
2023-05-10 14:13:05 -0700

    Modified SVG Strokes
    Modified SVG strokes for checkmark and xmark.

--- ComposureWatch/src/Components/SVG/CheckMarkSvg.jsx
+++ ComposureWatch/src/Components/SVG/CheckMarkSvg.jsx
@@ -6,7 +6,7 @@ const CheckMarkSvg = () => {
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
-      strokeWidth={3.5}
+      strokeWidth={4}
       stroke="#fff"
       className="w-[18px] h-[18px]"
     >
