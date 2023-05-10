1bda27ad06dd01c1db706c8f71a743baa5f4bdee
David Fagan <davida.fagan@gmail.com>
2023-05-10 13:01:19 -0700

    Created XMarkSVG.jsx

---
+++ ComposureWatch/src/Components/SVG/XMarkSvg.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function XMarkSvg() {
+  return (
+    <>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={3}
+        stroke="currentColor"
+        className="w-[18px] h-[18px]"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M6 18L18 6M6 6l12 12"
+        />
+      </svg>
+    </>
+  );
+}
+
+export default XMarkSvg;
