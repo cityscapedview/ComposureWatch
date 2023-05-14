4446ae5e272e579ee25f75ba1aacd3375cd7fd24
David Fagan <davida.fagan@gmail.com>
2023-05-13 19:13:57 -0700

    PlayerSuperCards.jsx Styled to Accept Props
    String literals created so that PlayerSuperCards can accept teams and apply themes based on those teams.

--- ComposureWatch/src/Components/Card/PlayerSuperCards.jsx
+++ ComposureWatch/src/Components/Card/PlayerSuperCards.jsx
@@ -44,9 +44,15 @@ const PlayerSuperCards = ({
       <div
         className={`border-4 ${theme.border} rounded-xl shadow-2xl ${theme.shadow} h-[227px] w-[175px] ${theme.background}`}
       >
-        <div className="border-2 border-[#2EF3FF] bg-[#0A0D30] rounded-lg h-[215px] w-[163px] m-0.5 flex flex-col justify-start items-center">
-          <div className="flex flex-col justify-start items-center border-2 bg-black border-[#2EF3FF] border-opacity-75 rounded-lg h-[185px] w-[155px] m-0.5 mb-12 text-[#2EF3FF] shadow-inner shadow-[#13336C]">
-            <div className="rounded-full shadow-xl shadow-[#13336C] mt-2 mx-8 mb-2">
+        <div
+          className={`border-2 ${theme.border} ${theme.background} rounded-lg h-[215px] w-[163px] m-0.5 flex flex-col justify-start items-center`}
+        >
+          <div
+            className={`flex flex-col justify-start items-center border-2 bg-black ${theme.border} border-opacity-75 rounded-lg h-[185px] w-[155px] m-0.5 mb-12 ${theme.text} shadow-inner ${theme.shadow}`}
+          >
+            <div
+              className={`rounded-full shadow-xl ${theme.shadow} mt-2 mx-8 mb-2`}
+            >
               <CharIcon character={character} />
             </div>
             <h2 className="italic text-xl leading-10">{name}</h2>
