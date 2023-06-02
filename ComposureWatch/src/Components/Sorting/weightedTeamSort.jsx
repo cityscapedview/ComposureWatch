e98c457e33c4ab6983144759e3e984100ad502ed
David Fagan <Davida.fagan@gmail.com>
2023-06-02 10:39:48 -0700

    Created weightedTeamSort Function

---
+++ ComposureWatch/src/Components/Sorting/weightedTeamSort.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const weightedTeamSort = () => {
+  // Shuffle Players
+
+  // Randomly assign roles into 3 objects
+
+  // weighted team average
+
+  // if team averages are not equal resort
+
+  return <div>weightedTeamSort</div>;
+};
+
+export default weightedTeamSort;
