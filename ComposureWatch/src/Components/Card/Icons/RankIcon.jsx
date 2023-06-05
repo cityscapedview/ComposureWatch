f5107768cd576f92c1f948c156c5c2f03ae3c885
David Fagan <Davida.fagan@gmail.com>
2023-06-05 10:31:46 -0700

    Simplified Rating Selection
    Rating selection is based off of a simple 1-7 scale.  This makes for a better user experience.

--- ComposureWatch/src/Components/Card/Icons/RankIcon.jsx
+++ ComposureWatch/src/Components/Card/Icons/RankIcon.jsx
@@ -9,145 +9,31 @@ import grandmaster from "../../../images/badge/grandmaster.webp";
 const RankIcon = ({ rank }) => {
   const rankList = [
     {
-      id: "bronze5",
+      id: "bronze",
       img: bronze,
     },
     {
-      id: "bronze4",
-      img: bronze,
-    },
-    {
-      id: "bronze3",
-      img: bronze,
-    },
-    {
-      id: "bronze2",
-      img: bronze,
-    },
-    {
-      id: "bronze1",
-      img: bronze,
-    },
-    {
-      id: "silver5",
+      id: "silver",
       img: silver,
     },
     {
-      id: "silver4",
-      img: silver,
-    },
-    {
-      id: "silver3",
-      img: silver,
-    },
-    {
-      id: "silver2",
-      img: silver,
-    },
-    {
-      id: "silver1",
-      img: silver,
-    },
-
-    {
-      id: "gold5",
+      id: "gold",
       img: gold,
     },
     {
-      id: "gold4",
-      img: gold,
-    },
-    {
-      id: "gold3",
-      img: gold,
-    },
-    {
-      id: "gold2",
-      img: gold,
-    },
-    {
-      id: "gold1",
-      img: gold,
-    },
-
-    {
-      id: "platinum5",
+      id: "platinum",
       img: platinum,
     },
     {
-      id: "platinum4",
-      img: platinum,
-    },
-    {
-      id: "platinum3",
-      img: platinum,
-    },
-    {
-      id: "platinum2",
-      img: platinum,
-    },
-    {
-      id: "platinum1",
-      img: platinum,
-    },
-    {
-      id: "diamond5",
+      id: "diamond",
       img: diamond,
     },
     {
-      id: "diamond4",
-      img: diamond,
-    },
-    {
-      id: "diamond3",
-      img: diamond,
-    },
-    {
-      id: "diamond2",
-      img: diamond,
-    },
-    {
-      id: "diamond1",
-      img: diamond,
-    },
-    {
-      id: "master5",
+      id: "master",
       img: master,
     },
     {
-      id: "master4",
-      img: master,
-    },
-    {
-      id: "master3",
-      img: master,
-    },
-    {
-      id: "master2",
-      img: master,
-    },
-    {
-      id: "master1",
-      img: master,
-    },
-    {
-      id: "grandMaster5",
-      img: grandmaster,
-    },
-    {
-      id: "grandMaster4",
-      img: grandmaster,
-    },
-    {
-      id: "grandMaster3",
-      img: grandmaster,
-    },
-    {
-      id: "grandMaster2",
-      img: grandmaster,
-    },
-    {
-      id: "grandMaster1",
+      id: "grandMaster",
       img: grandmaster,
     },
   ];
